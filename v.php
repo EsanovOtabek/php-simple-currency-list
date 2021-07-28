6a9b701eb931ddfb2a30cc7a08ba10ba04a29e2c
Esanov Otabek <esanovotabek688@gmail.com>
2021-07-28 10:31:42 +0500

    update currency list with nbu api

--- v.php
+++ v.php
@@ -1,33 +1,78 @@
+
+<? 
+
+$uri="https://nbu.uz/en/exchange-rates/json/";
+$json=json_decode(file_get_contents($uri),true);
+$valyuta=array_reverse($json);
+$i=1;
+ ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Valyuta Kursi LifePC</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valyuta</title>
+    <style>
+        table {
+          font-family: Arial, Helvetica, sans-serif;
+          border-collapse: collapse;
+          width: 100%;
+        }
 
-<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'/>
-<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        table td, table th {
+          border: 1px solid #ddd;
+          padding: 8px;
+        }
 
-<title>Valyutalar kursi</title>
+        table tr:nth-child(even){background-color: #f2f2f2;}
 
+        table tr:hover {background-color: #ddd;}
+
+        table th {
+          padding-top: 12px;
+          padding-bottom: 12px;
+          text-align: left;
+          background-color: #04AA6D;
+          color: white;
+        }
+    </style>
 </head>
 <body>
-<h1 align="center">Valyuta kurslari</h1>
-<?
-$str="";
-$link = file_get_contents("https://nbu.uz/uz/exchange-rates/index.php");
-echo '<div class="';
-for ($i=0; $i <strlen($link);$i++) { 	
-	if ($link[$i]=='e'&&$link[$i+1]=='x'&&$link[$i+2]=='c'&&$link[$i+3]=='h'&&$link[$i+4]=='_'&&$link[$i+5]=='t'&&$link[$i+6]=='a') {
-		$start=true;
-	}
-		if($start) $str.=$link[$i];
-	if ($link[$i]=='e'&&$link[$i+1]=='x'&&$link[$i+2]=='c'&&$link[$i+3]=='h'&&$link[$i+4]=='_'&&$link[$i+5]=='q'&&$link[$i+6]=='u') {
-		$start=false;
-	}
-}
-$str=str_replace("/local/templates", "https://nbu.uz/uz/exchange-rates/local/templates", $str);
-$str=str_replace("Boshqa kurslarni ko`rsatish", "Boshqa kurslar", $str);
-	echo $str;
-?>
+    <h2 align="center"><?=$json[0]['date']?></h2>
+    <table id="currency">
+    <tr>
+        <th> №</th>
+        <th> Code</th>
+        <th> Currency </th>
+        <th> Nbu Buy </th>
+        <th> Nbu Sell </th>
+        <th> Central Bank </th>
+    </tr>
+    <? foreach ($valyuta as $key => $currency) {?>
+        <tr>
+            <td><?=$i++?></td>
+            <td><?=$currency['code']?></td>
+            <td>1 <?=$currency['title']?></td>
+            <td><?
+                $x=trim($currency['nbu_buy_price']);
+                echo $x;
+                if(strlen($x)>0)echo" so'm";
+                ?>
+            </td>
+            <td><?
+                $x=trim($currency['nbu_buy_price']);
+                echo $x;
+                if(strlen($x)>0)echo" so'm";
+                ?>
+            </td>
+            <td><?=$currency['cb_price']?> so'm</td>
+        </tr>
+   <? } ?>
+
+   
+
+    </table>
+
 
 </body>
 </html>
